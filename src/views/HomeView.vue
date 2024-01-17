6528ef5573a2d5da65cfaa509e2f6a6e87782ace
Jaeri <motonimjrp@gmail.com>
2024-01-17 15:21:30 -0500

    set up data and homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="home">
-    Home
+    <div v-if="projects.length">
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project"/>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import SingleProject from '../components/SingleProject.vue';
+import SingleProject from '@/components/SingleProject.vue';
 
 export default {
   name: 'HomeView',
-  components: {
+  components: { SingleProject},
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
   }
 }
 </script>
