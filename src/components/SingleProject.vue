b33a451b0b439e081592225d8a5274bf110a9f28
Jaeri <motonimjrp@gmail.com>
2024-01-17 15:40:34 -0500

    delete projects both db.json and json-server

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -2,6 +2,11 @@
     <div class="project">
         <div class="actions">
             <h3 @click="toggleDetails">{{  project.title  }}</h3>
+            <div class="icons">
+                <span class="material-icons">edit</span>
+                <span class="material-icons" @click="deleteProject">delete</span>
+                <span class="material-icons">done</span>
+            </div>
         </div>
         <div class="details" v-if="showDetails">
             <p>{{  project.details  }}</p>
@@ -14,27 +19,47 @@ export default {
     props: ['project'],
     data() {
         return {
-            showDetails: false
+            showDetails: false,
+            uri: 'http://localhost:3000/projects/' + this.project.id
         }
     }, 
     methods: {
         toggleDetails() {
             this.showDetails = !this.showDetails
+        },
+        deleteProject() {
+            fetch(this.uri, { method: 'DELETE' })
+                .then(() => this.$emit('delete', this.project.id)) // in this case, we don't need any response because it's deleting an item.
+                .catch(err => console.log(err.message))
         }
     }
 }
 </script>
 
 <style scoped>
-.project {
+  .project {
     margin: 20px auto;
     background: white;
     padding: 10px 20px;
     border-radius: 4px;
     box-shadow: 1px 2px 3px rgba(0,0,0,0.05);
     border-left: 4px solid #e90074;
-}
-h3 {
+  }
+  h3 {
     cursor: pointer;
-}
+  }
+  .actions {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .material-icons {
+    font-size: 24px;
+    margin-left: 10px;
+    color: #bbb;
+    cursor: pointer;
+  }
+  .material-icons:hover {
+    color: #777;
+  }
 </style>
